3b1d1a77e069f8f9b37d138ff289924df5ad4883
j-osypiuk <kuba6535@gmail.com>
2024-03-15 17:25:28 +0100

    Setup cassandra project structure

---
+++ Cassandra/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import org.example.repository.RentRepository;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RentRepository rentRepository = new RentRepository();
+
+    }
+}
