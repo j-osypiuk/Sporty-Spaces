d834fdc9786b7820f6601a4b244c9f66376b96e4
j-osypiuk <kuba6535@gmail.com>
2024-03-27 15:33:05 +0100

    Create Main class

---
+++ Kafka/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.entityKafka.RentKfk;
+import org.example.kafka.Producer;
+
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+
+    }
+}
